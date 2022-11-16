ef3da55114e954eb0cfefc2e98324aa57be8d768
yerimechoi <yerimechoi@gmail.com>
2022-11-16 03:16:45 -0500

    Commented out API for now

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,1 @@
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c9e7827720d40ceda697937555df69aa
