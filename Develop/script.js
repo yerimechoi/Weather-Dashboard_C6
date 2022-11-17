035d18995cbf78401796eb50e91c065a86c9bc42
yerimechoi <yerimechoi@gmail.com>
2022-11-17 18:51:13 -0500

    Created files

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,43 @@
-// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=c9e7827720d40ceda697937555df69aa
+// TO DO
+// .ready() current location
+// search btn event listener
+// search city
+// save location & data to search history
+// add btn to the search history
+// presented with current city name, date, icon (depending on weather coniditions), temerpature, wind & humidity 
+// presented with future city name, date, icon (depending on weather coniditions), temerpature, wind & humidity 
+    // long&lat results = future
+
+var APIKey = "c9e7827720d40ceda697937555df69aa";
+
+//current city
+// $(document).ready(function(){
+// });
+
+$(`#searchBtn`).click(function(event){
+    event.preventDefault();
+
+    var city = $(".userInput").val().trim();
+    if (city === " "){
+        return alert('Enter a valid City Name');
+    }
+    searchWeather(city);
+});
+
+function searchWeather(city){
+    var searchURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+
+    $.ajax({
+        url: searchURL,
+        method: `GET`,
+    }).then(function(response){
+        console.log(response)
+    })
+}
+
+
+
+    // var longLatURL = `http://api.openweathermap.org/geo/1.0/reverse?lat=` + lat + `&lon=` + lon +`&limit=` + limit + `&appid=` + APIKey;
+    // var lat;
+    // var lon;
+    // var limit;
